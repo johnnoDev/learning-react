8a14fd4e60c254809335a418badb9ebb90691d7f
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-18 09:59:16 -0500

    aplicando css con un objeto literal

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,3 +1,5 @@
+import { CSSProperties } from "react";
+
 // Impresión de variables
 const firstName: string = 'Johnny'
 const lastName: string = 'López'
@@ -11,20 +13,22 @@ const Person = {
     'age': 22,
 }
 
+const styles: CSSProperties = {
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    padding: 30,
+    fontSize: 40,
+    font: 'small-caption'
+}
+
 export const MyAwesomeApp = () => {
     return (
         <>
             <h1>{ firstName }</h1>
             <h2>{ lastName }</h2>
-            <p style={{
-                backgroundColor: 'red',
-                borderRadius: 10,
-                padding: 30
-            }}>{ fruits.join(', ').toUpperCase()}</p>
+            <p style={styles}>{ fruits.join(', ').toUpperCase()}</p>
             <h3>{ isActive ? 'Activo' : 'Inactivo' }</h3>
             <h4>{ JSON.stringify(Person)}</h4>
-
-
         </>
     )
 }
