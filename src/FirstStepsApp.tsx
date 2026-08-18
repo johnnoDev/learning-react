0e9b0ff8696ae26210ed6fd557cfb2483bd18d0a
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-18 12:05:35 -0500

    Mostrar listado de elementos; además, se corrigió algunos errores de componentes que se ubicaban en una dirección errónea

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,8 +1,29 @@
+import { ItemCounterApp } from "./shopping-cart/itemCounterApp";
+
+interface ItemsInCar {
+    productName: string,
+    quantity: number,
+}
+
+// Lista de objetos
+const itemsInCar: ItemsInCar[] = [
+    {productName: 'Nintendo Switch', quantity: 20},
+    {productName: 'Pro Controller', quantity: 50},
+    {productName: 'Super Smash', quantity: 12},
+]
+
 export function FirsStepsApp() {
     return (
         <>
-            <h1>Hola mundo desde firstSteps</h1>
-            <button>click me</button>
+            <h1>Carrito de Compra</h1>
+            {
+                itemsInCar.map( ({productName, quantity}) => (
+                    <ItemCounterApp name={productName} quantity={quantity}/>
+                ))
+            }
+            {/* <ItemCounterApp name='Nintendo Switch' quantity={50}/>
+            <ItemCounterApp name='Pro controller' quantity={20}/>
+            <ItemCounterApp name='Super Smash' quantity={30}/> */}
         </>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { MyAwesomeApp } from './MyAwesomeApp';
+import { FirsStepsApp } from './FirstStepsApp';
 
 createRoot(document.getElementById('root')!).render(
 <StrictMode>
-  <MyAwesomeApp/>
+  <FirsStepsApp />
 </StrictMode>
 )
